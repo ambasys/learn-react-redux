9749a09e16b28475bd135df05e74dd1d0a675732
ambaw <ambmulat897@gmail.com>
2024-09-23 23:28:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './Home'
+import About from './About'
+import NewPost from "./NewPost";
+import Header from "./Header";
+import PostPage from "./PostPage";
+import Footer from "./Footer";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import EditPost from "./EditPost";
+import { useEffect } from "react";
+import useAxiosFetch from "./hooks/useAxiosFetch";
+import { useStoreActions } from "easy-peasy";
 function App() {
+  const setPosts=useStoreActions((actions)=>actions.setPosts);
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+
+  useEffect(() => {
+      setPosts(data);
+  }, [data,setPosts])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <Header title="React JS Blog" />
+          <Nav/>
+          <Routes>
+            <Route path="/" element={
+              <Home
+                isLoading={isLoading}
+                fetchError={fetchError}
+                />}/>
+            <Route path="post" element={<NewPost/>} />
+            <Route path="edit/:id" element={<EditPost />} />
+
+            <Route path="/post/:id" element={<PostPage />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Missing/>} />
+          </Routes>
+          
+          <Footer />
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
